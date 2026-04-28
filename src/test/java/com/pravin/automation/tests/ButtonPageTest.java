9ccb092d670ef9d18db387545f084110f087869f
git-pravin06 <108776265+git-pravin06@users.noreply.github.com>
2026-04-28 10:56:56 +0530

    feat:Add loggin in button page for debug

--- src/test/java/com/pravin/automation/tests/ButtonPageTest.java
+++ src/test/java/com/pravin/automation/tests/ButtonPageTest.java
@@ -2,22 +2,28 @@ package com.pravin.automation.tests;
 
 import com.pravin.automation.base.BaseTest;
 import com.pravin.automation.pages.ButtonPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ButtonPageTest extends BaseTest {
+    private static final Logger log = LogManager.getLogger(MouseEventTest.class);
     ButtonPage buttonPage;
 
     @BeforeMethod
         public void openURL(){
+        log.info("Launching URL");
         driver.get("https://www.qa-practice.com/elements/button/simple");
         buttonPage = new ButtonPage(driver);
     }
 
     @Test
         public void click_shouldclick (){
+        log.info("Clicking on button");
         buttonPage.clickbutton();
+        log.info("Clicked on button and getting text ");
         buttonPage.getresulttext();
         String actualClickMessage = buttonPage.getresulttext();
         Assert.assertTrue(actualClickMessage.contains("Submitted"));
